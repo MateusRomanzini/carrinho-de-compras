5d6272bffb6b7cbba13912a42509ca91b922b724
Mateus Romanzini <romanzinimateus@gmail.com>
2024-01-16 23:25:14 -0300

    the last

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -2,12 +2,18 @@ let totalGeral = 0
 document.getElementById('lista-produtos').innerHTML = '';
 document.getElementById('valor-total').innerHTML = 'R$ 0';
 
+let listaDeProdutosPermitidos = ["Fone de ouvido", "Celular", "Oculus VR"];
+
 function adicionar() {
     // Recuperar valores do nome do produto, quantidade e do valor
     let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0];
+    if (listaDeProdutosPermitidos.includes(nomeProduto)) {
     let valorUnitario = produto.split('R$')[1];
     let quantidade = document.getElementById('quantidade').value;
+    if (isNaN(quantidade) || quantidade <= 0) {
+      alert("Quantidade inválida. Insira uma quantidade válida.");
+    }
     // Calcular o preço, o nosso subtotal 
     let preco = quantidade * valorUnitario;
     // Adicionar no carrinho 
@@ -20,6 +26,9 @@ function adicionar() {
     let campoTotal = document.getElementById('valor-total');
     campoTotal.textContent = `R$ ${totalGeral}`;
     document.getElementById('quantidade').value = 0;
+  } else {
+    alert("Produto inválido. Selecione um produto da lista permitida.");
+  }
 }
 
 
